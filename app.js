44b84f624cea904e99d2f549292e19134ec6fc75
Javi <javisan5712@gmail.com>
2026-07-11 21:24:47 +0200

    se añade routes de clients

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 
 import router from './routes/products.routes.js';
+import clientrouter from './routes/client.routes.js';
 import { testDbConnection } from './config/db.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -11,7 +12,7 @@ const port = Number(process.env.PORT || 3000);
 
 app.use(express.json());
 app.use('/api/products', router);
-
+app.use('/api/clients', clientrouter);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
